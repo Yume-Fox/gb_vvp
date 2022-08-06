ee9c7bd1fbac446b6164a4288aceb1c24d06ac5a
YumeFox <yumefox@mail.ru>
2022-08-06 12:51:32 +0300

    added folder

---
+++ gb_vvp_L02/Program.cs
@@ -0,0 +1,20 @@
+﻿/* int num1 = 5;
+int num2 = 3;
+Console.Write("Результат: " + (num1 + num2)); */
+
+
+/* int num1 = new Random().Next(1, 35);
+Console.WriteLine("Первое число: " + num1);
+int num2 = new Random().Next(1, 35);
+Console.WriteLine("Второе число: " + num2);
+Console.Write("Результат: " + (num1 + num2)); */
+
+
+Console.Write("Введите ваше имя: ");
+string username = Console.ReadLine();
+if (username.ToLower() == "мария") {
+    Console.Write("Омай гарбл, Данила! Это же " + username);
+}
+else {
+    Console.Write("Привет, " + username);
+}
