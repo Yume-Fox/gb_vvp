2e6b4e05ab32ea15c0bd0f0ea7c758daf3009ab5
YumeFox <yumefox@mail.ru>
2022-08-11 17:40:48 +0300

    nothing important

--- gb_sem/Program.cs
+++ gb_sem/Program.cs
@@ -2,16 +2,16 @@
 
 // Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
 
-Console.Write("Введите первое число: ");
-int num1 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите второе число: ");
-int num2 = Convert.ToInt32(Console.ReadLine());
-if(num1 > num2){
-    Console.Write("Число " + num1 + " больше числа " + num2);
-}
-else {
-    Console.Write("Число " + num2 + " больше числа " + num1);
-}
+// Console.Write("Введите первое число: ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите второе число: ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+// if(num1 > num2){
+//     Console.Write("Число " + num1 + " больше числа " + num2);
+// }
+// else {
+//     Console.Write("Число " + num2 + " больше числа " + num1);
+// }
 
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// 
